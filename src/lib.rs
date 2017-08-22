53181020f810348f0fae73f285cf2ce577f92c45
Anton Tiurin <noxiouz@yandex.ru>
2017-08-22 15:42:41 +0300

    bugfix: fix a couple of bugs

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,15 @@ use std::hash::{Hash, Hasher};
 
 use siphasher::sip::SipHasher;
 
+pub const SMALL_M: usize = 65537;
+pub const BIG_M: usize = 655373;
+
 pub struct Maglev<T: Hash + PartialEq + Copy> {
+    // VIPs added
     nodes: Vec<T>,
+
+    // size of lookup table
+    m: usize,
 
     lookup: Vec<i64>,
     permutations: Vec<Vec<usize>>,
@@ -15,39 +22,42 @@ pub struct Maglev<T: Hash + PartialEq + Copy> {
 const SIP_OFFSET_KEY: u64 = 0xdeadbabe;
 const SIP_SKIP_KEY: u64 = 0xdeadbeef;
 
-impl<T> Maglev<T> where T: Hash + PartialEq + Copy {
-    pub fn new() -> Maglev<T> {
-        let mut m = Maglev{
+impl<T> Maglev<T>
+where
+    T: Hash + PartialEq + Copy,
+{
+    pub fn new(m: usize) -> Maglev<T> {
+        let mut mh = Maglev {
             nodes: Vec::new(),
+            m: m,
 
             lookup: Vec::new(),
             permutations: Vec::new(),
         };
 
-        m.generate_population();
-        m.populate();
-        m
+        mh.generate_population();
+        mh.populate();
+        mh
     }
 
     fn generate_population(&mut self) {
         if self.nodes.is_empty() {
-            return
+            return;
         }
 
         for node in &self.nodes {
-            let modulo = self.lookup.len();
             let mut s = SipHasher::new_with_keys(SIP_OFFSET_KEY, 0);
             node.hash(&mut s);
-            let offset: usize = s.finish() as usize % modulo;
+            let offset: usize = s.finish() as usize % self.m;
 
             let mut s = SipHasher::new_with_keys(SIP_SKIP_KEY, 0);
             node.hash(&mut s);
-            let skip: usize = (s.finish() as usize % (modulo - 1)) + 1;
+            let skip: usize = (s.finish() as usize % (self.m - 1)) + 1;
 
             // TODO: iter + extend
-            let mut row : Vec<usize> = Vec::new();
-            for j in 0usize..modulo {
-                row.push((offset + j * skip) % modulo);
+            let mut row: Vec<usize> = Vec::new();
+            for j in 0usize..self.m {
+                row.push((offset + j * skip) % self.m);
             }
 
             self.permutations.push(row);
@@ -56,28 +66,27 @@ impl<T> Maglev<T> where T: Hash + PartialEq + Copy {
 
     fn populate(&mut self) {
         if self.nodes.is_empty() {
-            return
+            return;
         }
 
         let mut next: Vec<usize> = vec![0; self.nodes.len()];
-        let mut entry: Vec<i64> = vec![-1; self.lookup.len()];
+        let mut entry: Vec<i64> = vec![-1; self.m];
         let mut n = 0usize;
 
         loop {
-            for i in 0..self.lookup.len() {
+            for i in 0..self.nodes.len() {
                 let mut c = self.permutations[i][next[i]];
                 while entry[c] >= 0 {
-                    next[i]+=1;
+                    next[i] += 1;
                     c = self.permutations[i][next[i]];
                 }
 
                 entry[c] = i as i64;
                 next[i] += 1;
-                n+=1;
-
-                if n == self.lookup.len() {
+                n += 1;
+                if n == self.m {
                     self.lookup = entry;
-                    return
+                    return;
                 }
             }
         }
@@ -88,7 +97,7 @@ impl<T> Maglev<T> where T: Hash + PartialEq + Copy {
     // TODO: clone?
     pub fn add(&mut self, item: T) {
         if self.nodes.contains(&item) {
-            return
+            return;
         }
 
         self.nodes.push(item);
@@ -99,9 +108,9 @@ impl<T> Maglev<T> where T: Hash + PartialEq + Copy {
     // TODO: return error
     pub fn remove(&mut self, item: &T) {
         match self.nodes.iter().position(|x| x == item) {
-            Some(index) =>{
+            Some(index) => {
                 self.nodes.swap_remove(index);
-            },
+            }
             None => return,
         };
 
@@ -110,8 +119,8 @@ impl<T> Maglev<T> where T: Hash + PartialEq + Copy {
     }
 
     pub fn get(&self, item: &T) -> Option<T> {
-        if !self.nodes.contains(&item) {
-            return None
+        if self.nodes.is_empty() {
+            return None;
         }
 
         let mut s = SipHasher::new_with_keys(SIP_OFFSET_KEY, 0);
@@ -124,12 +133,22 @@ impl<T> Maglev<T> where T: Hash + PartialEq + Copy {
 #[cfg(test)]
 mod tests {
     use Maglev;
+    use SMALL_M;
     #[test]
-    fn it_works() {
-        let mut m: Maglev<&str> = Maglev::new();
-        m.add("BACKEND1");
-        m.get(&"IP1");
-        m.get(&"IP2");
-        m.get(&"IP3");
+    fn fair_distribution() {
+        let size: i64 = 10;
+
+        let mut m: Maglev<i64> = Maglev::new(SMALL_M);
+        for i in 0..size {
+            m.add(i)
+        }
+
+        let mut r = vec![0, size];
+        for j in 0..10000 {
+            match m.get(&j) {
+                Some(index) => (),
+                None => (),
+            }
+        }
     }
 }
